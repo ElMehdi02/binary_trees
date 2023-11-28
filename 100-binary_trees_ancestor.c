c3189652411a7dcdf8b03dd1ecf295aed492cf4f
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-11-28 13:30:11 +0100

    binary_trees

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,15 +10,15 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-    binary_tree_t *mom, *pop;
+	binary_tree_t *mom, *pop;
 
-    if (!first || !second)
+	if (!first || !second)
 		return (NULL);
-    if (first == second)
+	if (first == second)
 		return ((binary_tree_t *)first);
 
-    mom = first->parent, pop = second->parent;
-    if (first == pop || !mom || (!mom->parent && pop))
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
 		return (binary_trees_ancestor(first, pop));
 	else if (mom == second || !pop || (!pop->parent && mom))
 		return (binary_trees_ancestor(mom, second));
